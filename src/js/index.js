adf8df619bdaff8b8318523beb529239d66d03ab
CeliaGallegoGomez <cela17@hotmail.es>
2024-05-17 17:20:51 +0200

    UPDATE

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
-const number = 5;
-const numbers= [13,25,34,46,56];
-console.log (numbers.math.random);
+const numbers =[1,2,3,4,5];
+
+const randomNumber=3;
+
+console.log (numbers[randomNumber]);
+
+const theOperations = numbers=> {
+    const sum =numbers[0]+numbers[1]+numbers[2];
+    const media =(numbers[0]+numbers[1]+numbers[2]) /2;
+    console.log(`la suma de todos los números es ${sum}`)
+    console.log(`la media de todos los números es ${media}`)
+}
+
+theOperations([6,7,8])
 
