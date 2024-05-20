146a5f3d114d04159a5129a2be1475c57474d3b6
CeliaGallegoGomez <cela17@hotmail.es>
2024-05-20 18:50:12 +0200

    UPDATE

--- src/js/index.js
+++ src/js/index.js
@@ -14,20 +14,20 @@ const theOperations = (numbers) => {
   console.log(`la suma de todos los números es ${sum}`);
   const media = sum / numbers.length;
   console.log(`la media de todos los números es ${media}`);
-  if (numbers[0] >= numbers[1] && numbers[0] >= numbers[2]) {
+  if (numbers[0] > numbers[1] && numbers[0] > numbers[2]) {
     mas = numbers[0];
-  } else if (numbers[1] >= numbers[0] && numbers[1] >= numbers[2]) {
+  } else if (numbers[1] > numbers[0] && numbers[1] > numbers[2]) {
     mas = numbers[1];
   } else {
-    max = number[2];
+    max = numbers[2];
   }
 
-  if (numbers[0] <= numbers[1] && numbers[0] <= numbers[2]) {
+  if (numbers[0] < numbers[1] && numbers[0] < numbers[2]) {
     min = numbers[0];
-  } else if (numbers[1] <= numbers[0] && numbers[1] <= numbers[2]) {
+  } else if (numbers[1] < numbers[0] && numbers[1] < numbers[2]) {
     min = numbers[1];
   } else {
-    min = number[2];
+    min = numbers[2];
   }
   console.log(`el número menor es ${min} y el número mayor es ${max}`);
 };
@@ -44,7 +44,81 @@ const aleatoryNumbers = (nums) => {
         random
       )}`
     );
+  } else {
+    console.log(`El número ${random} no está en el array`);
   }
   console.log(numsContent);
 };
 aleatoryNumbers([2, 6, 8, 9, 3]);
+
+const blankArray = (array) => {
+  const randomNumbA = Math.floor(Math.random() * 101);
+  const randomNumbB = Math.floor(Math.random() * 101);
+  const randomNumbC = Math.floor(Math.random() * 101);
+  array.push(randomNumbA, randomNumbB, randomNumbC);
+  console.log(array);
+};
+blankArray([]);
+
+const newNumbers = (numbers) => {
+  const even = [];
+  const odd = [];
+  const randomNew = Math.floor(Math.random() * 11);
+  if ((numbers[0] * randomNew) % 2 === 0) {
+    even.push(numbers[0] * randomNew);
+  } else {
+    odd.push(numbers[0] * randomNew);
+  }
+  if ((numbers[1] * randomNew) % 2 === 0) {
+    even.push(numbers[1] * randomNew);
+  } else {
+    odd.push(numbers[1] * randomNew);
+  }
+  if ((numbers[2] * randomNew) % 2 === 0) {
+    even.push(numbers[2] * randomNew);
+  } else {
+    odd.push(numbers[2] * randomNew);
+  }
+  if ((numbers[3] * randomNew) % 2 === 0) {
+    even.push(numbers[3] * randomNew);
+  } else {
+    odd.push(numbers[3] * randomNew);
+  }
+  if ((numbers[4] * randomNew) % 2 === 0) {
+    even.push(numbers[4] * randomNew);
+  } else {
+    odd.push(numbers[4] * randomNew);
+  }
+
+  console.log(`${even} son pares`);
+  console.log(`${odd} son impares`);
+};
+
+newNumbers([3, 6, 9, 2, 4]);
+
+const dniLetter = (syla) => {};
+dniLetter([
+  "T",
+  "R",
+  "W",
+  "A",
+  "G",
+  "M",
+  "Y",
+  "F",
+  "P",
+  "D",
+  "X",
+  "B",
+  "N",
+  "J",
+  "Z",
+  "S",
+  "Q",
+  "V",
+  "H",
+  "L",
+  "C",
+  "K",
+  "E",
+]);
